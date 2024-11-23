fb566fda3a4b4d2830d57822e5712832b24874f9
Muntahamasuma <knockmasuma@gmail.com>
2024-11-23 18:26:41 +0600

    Updates the wishlist field in the userCollection in MongoDB

--- index.js
+++ index.js
@@ -11,9 +11,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// token verification
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.send({ message: "No Token" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.send({ message: "Invalid Token" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+// verify seller
+const verifySeller = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  if (user?.role !== "seller") {
+    return res.send({ message: "Forbidden access" });
+  }
+  next();
+};
+
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6dfffw4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +54,6 @@ const client = new MongoClient(uri, {
 const userCollection = client.db("Jesco-web").collection("users");
 const productCollection = client.db("Jesco-web").collection("products");
 
-// add product
-app.post("/addproducts", async (req, res) =>{
-  const product = req.body;
-  const result = await productCollection.insertOne(product);
-  res.send(result);
-})
 
 async function run() {
   try {
@@ -41,6 +61,38 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    // Get User
+
+    app.get("/user/:email", async (req, res) => {
+      const query = { email: { $regex: req.params.email, $options: "i" } };
+
+      console.log(req.params.email)
+      const user = await userCollection.findOne(query);
+      if(!user){
+        return res.send({message: "No User Found"})
+      }
+      res.send(user);
+    });
+
+    // insert users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const exitingUser = await userCollection.findOne(query);
+      if (exitingUser) {
+        return res.send({ message: "This user has already exist" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // add product
+    app.post("/addproducts", async (req, res) =>{
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    })
 
     app.get('/addproducts', async (req, res) => {
         
@@ -87,6 +139,17 @@ async function run() {
 
       res.json({ products, totalProducts, categories });
     });
+
+    // 
+    app.patch('/wishlist/add', async (req, res) => {
+      const {userEmail, productId} = req.body;
+
+      const result = await userCollection.updateOne(
+        {email: userEmail},
+        {$addToSet: {wishlist: new ObjectId(String(productId))}}
+      );
+      res.send(result)
+    })
 
   } finally {
     // Ensures that the client will close when you finish/error
